b52bf0615877b5298be2fd12c05adde9fd77f4af
Aleksandrs Jakubovskis <ximik002@gmail.com>
2023-12-19 18:49:05 +0200

    Additional elements in Base page for Scenario 2

--- src/pages/common/base.page.js
+++ src/pages/common/base.page.js
@@ -12,6 +12,34 @@ class BasePage {
     get snackBarLanguageChange() {
         return browser.$('.mat-simple-snack-bar-content');
     }
+
+    get accountButton() {
+        return browser.$('//button[@id="navbarAccount"]');
+    }
+
+    get loginButton() {
+        return browser.$('//button[@id="navbarLoginButton"]');
+    }
+
+    get userProfileButton() {
+        return browser.$('button[aria-label="Go to user profile"]');
+    }
+
+    get basketButton() {
+        return browser.$('[routerlink="/basket"]');
+    }
+
+    get userOrdersAndPaymentsButton() {
+        return browser.$('button[aria-label="Show Orders and Payment Menu"]') ;
+    }
+
+    get paymentOptionsButton() {
+        return browser.$('button[aria-label="Go to saved payment methods page"]'); 
+    }
+
+    get savedAddressButton() {
+        return browser.$('button[aria-label="Go to saved address page"]');
+    }
 }
 
 module.exports = BasePage;
