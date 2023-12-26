91df96dd7f9d36275a3a6a7e0feb2954f292f5db
Aleksandrs Jakubovskis <ximik002@gmail.com>
2023-12-26 16:01:24 +0200

    Config files for different browsers and parallel test execution

---
+++ config/wdio.conf-firefox.js
@@ -0,0 +1,34 @@
+const {getHeadlessArgs } = require('./arg.helper');
+const merge = require('deepmerge');
+const baseConfig = require('./wdio.conf-base');
+
+exports.config = merge(baseConfig.config, {
+    // ============
+    // Capabilities
+    // ============
+    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+    // time. Depending on the number of capabilities, WebdriverIO launches several test
+    // sessions. Within your capabilities you can overwrite the spec and exclude options in
+    // order to group specific specs to a specific capability.
+    //
+    // First, you can define how many instances should be started at the same time. Let's
+    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // files and you set maxInstances to 10, all spec files will get tested at the same time
+    // and 30 processes will get spawned. The property handles how many capabilities
+    // from the same test should run tests.
+    //
+    maxInstances: 10,
+    //
+    // If you have trouble getting all important capabilities together, check out the
+    // Sauce Labs platform configurator - a great tool to configure your capabilities:
+    // https://saucelabs.com/platform/platform-configurator
+    //
+    capabilities: [{
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                args: [].concat(getHeadlessArgs())
+            }
+        }
+    ]
+})
